e927e0e47233f24fdfb182586f7deb04f97d5ef4
Ludmilla Lopes <ludmilla.lopes@hotmail.com>
2021-09-06 01:28:33 -0300

    Ajustes em todo o código

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # kaza_viva
-Web site da marca Kaza Viva.
+Página inicial do website da marca Kaza Viva, desenvolvido pelo UI Designer [Bruno Azeredo](https://www.linkedin.com/in/brunopaivaazeredo/).
+
+![Screenshot do website Kaza Viva - desktop.](https://i.ibb.co/TcK2sSR/Kaza-Viva-desktop.jpg)![Screenshot do website Kaza Viva - mobile.](https://i.ibb.co/Jnx7Ncm/Kaza-viva-mobile.jpg)
+
+
+## Tecnologias
+
+* HTML5
+* CSS3
+* JavaScript
+* Web Design Responsivo
+* Flexbox
--- script/main.js
+++ script/main.js
@@ -1,43 +1,61 @@
-var balls = document.querySelector('.balls')
-var quant = document.querySelectorAll('.slides .image')
-var atual = 0
-var imagem = document.getElementById('atual')
-var rolar = true
+var balls = document.querySelector('.balls');
+var quant = document.querySelectorAll('.slides .image');
+var atual = 0;
+var imagem = document.getElementById('atual');
+var rolar = true;
+var intElemOffsetHeight = imagem.offsetHeight;
 
-for(let i=0; i < quant.length; i++){
-    var div = document.createElement('div')
-    div.id = i
-    balls.appendChild(div)
+for (let i = 0; i < quant.length; i++) {
+  var div = document.createElement('div');
+  div.id = i;
+  balls.appendChild(div);
 }
-document.getElementById('0').classList.add('imageAtual')
+document.getElementById('0').classList.add('imageAtual');
 
-var pos = document.querySelectorAll('.balls div')
+var pos = document.querySelectorAll('.balls div');
 
-for(let i=0; i < pos.length; i++){
-    pos[i].addEventListener('click', ()=> {
-        atual = pos[i].id
-        rolar = false
-        slide()
-    })
+for (let i = 0; i < pos.length; i++) {
+  pos[i].addEventListener('click', () => {
+    atual = pos[i].id;
+    rolar = false;
+    slide()
+  })
 }
 
-function slide(){
-    if(atual >= quant.length){
-        atual = 0
-    }
-    else if(atual < 0){
-        atual = quant.length--
-    }
-    document.querySelector('.imageAtual').classList.remove('imageAtual')
-    imagem.style.marginBottom = -715*atual+'px'
-    document.getElementById(atual).classList.add('imageAtual')
+function slide() {
+  if (atual >= quant.length) {
+    atual = 0;
+  }
+  else if (atual < 0) {
+    atual = quant.length--;
+  }
+  document.querySelector('.imageAtual').classList.remove('imageAtual');
+  imagem.style.marginBottom = -intElemOffsetHeight * atual + 'px';
+  document.getElementById(atual).classList.add('imageAtual');
 }
-setInterval(()=>{
-    if(rolar){
-        atual++
-        slide()
-    }
-    else{
-        rolar = true
-    }
-},4000)
+setInterval(() => {
+  if (rolar) {
+    atual++;
+    slide();
+  }
+  else {
+    rolar = true;
+  }
+}, 4000);
+
+
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu(event) {
+  const nav = document.getElementById('nav');
+  nav.classList.toggle('active');
+  const active = nav.classList.contains('active');
+  event.currentTarget.setAttribute('aria-expanded', active);
+  if (active) {
+    event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+  } else {
+    event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+  }
+}
+
+btnMobile.addEventListener('click', toggleMenu);
